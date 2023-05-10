1c0eeb0c46993c1fee5fe84a04e720dc440d43f8
Akonedev <akonedev@gmail.com>
2023-05-10 19:20:53 +0200

    Add code for search Form  to js files

--- js/script.js
+++ js/script.js
@@ -6,9 +6,36 @@ addBookButton();
 function addBookButton() {
     let addButton = document.createElement("div");
     addButton.innerHTML = `<div class="addBook">
-        <button onclick="" type="button" class="addButton"> Ajouter un livre </button>
+        <button onclick="addSearchForm()" type="button" class="addButton"> Ajouter un livre </button>
       </div>`;
       bookcontainer.appendChild(addButton);
       newQuery.after(addButton);
     }
+  
+// mise en page 
+function addSearchForm() {
+    const addBookDiv = document.querySelector(".addBook");
+    addBookDiv.innerHTML = `
+    <form id="search-card" onsubmit="searchResults(); return false;">
+      <div class="form-group">
+        <label class="bookTitle" for="title"> Titre du Livre </label>
+      
+        <input class="row-s-8 form-control" type="text" name="title" id="title" placeholder="Titre" > </br>
+      
+        <label class="bookAuthor" for="author">Auteur</label>
+    
+        <input class="form-control" type="text" name="author" id="author" placeholder="Auteur" > 
+      
+        <div class="button2"><br>
+          <button type="button" id="searchButton" class="searchButton"> Rechercher </button>
+        </div><br>
+  
+        <div class="button3">
+          <button type="button" id="cancelButton" class="cancelButton"> Annuler </button>
+        </div><br>
+      </div>
+    </form>`;
+    
+    
+  }
   
